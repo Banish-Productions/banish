06b8570b69d0e95cdf2f30737785e194bbbd8fa9
educationeating <79743110+educationeating@users.noreply.github.com>
2022-10-05 09:58:48 +0530

    Add files via upload

---
+++ precache-manifest.4a07d8115918acde32822e49ee43b31b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c51dd430f83b57fdd161",
+    "url": "./static/css/main.c6148a1f.chunk.css"
+  },
+  {
+    "revision": "c51dd430f83b57fdd161",
+    "url": "./static/js/main.c51dd430.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "./static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "529ae0e3bae1e0b7b4fd",
+    "url": "./static/css/2.44613fab.chunk.css"
+  },
+  {
+    "revision": "529ae0e3bae1e0b7b4fd",
+    "url": "./static/js/2.529ae0e3.chunk.js"
+  },
+  {
+    "revision": "ca2150d5ae968420089a",
+    "url": "./static/js/runtime~main.ca2150d5.js"
+  },
+  {
+    "revision": "ae0d936e6ab5c704eaf8ebecb873e97a",
+    "url": "./static/media/favicon.ae0d936e.png"
+  },
+  {
+    "revision": "99300aeec7c31bd38a38559a26a71bd6",
+    "url": "./static/media/md404.99300aee.png"
+  },
+  {
+    "revision": "44df96b5c5a86799fcaa62b741aafa99",
+    "url": "./static/media/ee.44df96b5.png"
+  },
+  {
+    "revision": "c78127441672c8dcf10e3e64c0891469",
+    "url": "./static/media/db.c7812744.ico"
+  },
+  {
+    "revision": "3a90e160e5a2d2cc7c4959407e2b139b",
+    "url": "./static/media/fdl.3a90e160.jpg"
+  },
+  {
+    "revision": "d5bfced616fe28d2cc383654332efbb4",
+    "url": "./index.html"
+  }
+];
